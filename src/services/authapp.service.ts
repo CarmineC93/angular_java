684a439106679a251fec27507c37fa0e8bfed0db
CarmineC93 <carmine.caglia93@gmail.com>
2024-01-10 14:48:20 +0100

    generate logout component and function

--- src/services/authapp.service.ts
+++ src/services/authapp.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { SessionState } from 'http2';
 
 @Injectable({
   providedIn: 'root'
@@ -8,6 +9,20 @@ export class AuthappService {
   constructor() { }
 
   autentica = (userId: string, password: string) : boolean => {
-    return (userId === 'Carmine' && password === '123') ? true : false;
+    var retVar = (userId === 'Carmine' && password === '123') ? true : false;
+    //per salvare il dato dell'utente nel session storage
+    if(retVar){
+      sessionStorage.setItem("Utente", userId);
+    }
+    return retVar;
   }
+
+  loggedUser = () : string | null => (sessionStorage.getItem("Utente")) ? sessionStorage.getItem("Utente") : "";
+
+  isLogged = () : boolean => (sessionStorage.getItem("Utente")) ? true : false;
+
+  clearUser = () : void => sessionStorage.removeItem("Utente");
+
+  cleraAll  = () : void => sessionStorage.clear();
+
 }
